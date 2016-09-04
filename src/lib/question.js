66c25c29b433f6357bbfa2467fd9e5bed91f6032
Ishi Ruy <dev@nhz.io>
2016-09-04 05:37:33 +0300

    added Question class and related interfaces

---
+++ src/lib/question.js
@@ -0,0 +1,64 @@
+// src/lib/question.js @flow
+
+import inquirer from 'inquirer'
+import pick from 'lodash.pick'
+import clone from 'lodash.clonedeep'
+
+import {IAnswer, Answer} from './answer'
+
+export interface IChoice {
+	name: string;
+	value: any;
+	short: string;
+}
+
+export interface IQuestionData {
+	type: string;
+	name: string;
+	message: string | Function;
+	default?: string | number | Array<any> | Function;
+	choices?: Array<string | IChoice> | Function;
+	validate?: Function;
+	filter?: Function;
+	when?: Function | boolean;
+}
+
+export interface IQuestion extends IQuestionData {
+	constructor(args: IQuestionData) : IQuestion;
+	data(source?: IQuestionData): IQuestionData;
+	ask(): Promise<IAnswer>;
+}
+
+export class Question {
+	type: string;
+	name: string;
+	message: string | Function;
+	default: string | number | Array<any> | Function;
+	choices: Array<string | IChoice> | Function;
+	validate: Function;
+	filter: Function;
+	when: Function | boolean;
+
+	constructor(args: IQuestionData) {
+		Object.assign((this: Object), this.data(args))
+	}
+
+	/** Clone the question (or passed in source) data as an Object */
+	data(source?: IQuestionData) : IQuestionData {
+		return clone(pick(source || this, [
+			'type', 'name', 'message', 'default',
+			'choices', 'validate', 'filter', 'when'
+		]))
+	}
+
+	/** Prompt the question with inquirer returning promised answer */
+	ask() : Promise<IAnswer> {
+		return inquirer.prompt(this.data()).then(a =>
+			new Answer({key: this.name, value: a[this.name]})
+		)
+	}
+}
+
+export default function q(args: IQuestionData) : IQuestion {
+	return new Question(args)
+}
